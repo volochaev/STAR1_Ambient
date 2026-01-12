3412677694e1a11a77d7c0e78f1f3e12e6d028c7
Nikolai Volochaev <nik@funriver.net>
2026-01-12 23:13:27 +0300

    Add gitignore))

--- Debug/Lightning/presets.d
+++
@@ -1,5 +0,0 @@
-Lightning/presets.o: ../Lightning/presets.c ../Lightning/presets.h \
- ../Lightning/types.h ../Lightning/palette.h
-../Lightning/presets.h:
-../Lightning/types.h:
-../Lightning/palette.h:
